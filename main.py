c6ac396c0387b0128318a6357ffa039c04fdf1a4
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-09-10 12:11:02 -0400

    added testing func and fixed msg not sending content

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ class myClient(discord.Client):
         bot_channel = discord.utils.get(all_channels_iterator, name=bot_channel_name)
         global bot_channel_id
         bot_channel_id = bot_channel.id
+        await set_channels_for_testing(bot_channel)
 
     async def on_message(self, msg: discord.Message):
         in_bot_channel = msg.channel.name == bot_channel_name
         in_dictionary_channel = msg.channel.name == "dictionary"
         in_phrases_channel = msg.channel.name == phrases
         in_history_channel = msg.channel.name == history
-        # if msg.author == client.user:
-        #     return
-
+        if msg.author == client.user:
+            return
         if in_bot_channel:
             await set_channel(msg)
         elif in_dictionary_channel:
@@ -36,7 +36,7 @@ class myClient(discord.Client):
 
 
 async def reformat_dictionary_input(msg: discord.Message):
-    if re.search('[1-9]*(?=.)(?=[a-zA-Z]+(?=:)(?=[1-9]*(?=.)(?=[a-zA-Z, ]+)))+$', msg.content):
+    if re.search('[a-zA-Z]+(?=:)(?=[1-9]*(?=.)(?=[a-zA-Z, ]+))+$', msg.content):
         msgArray = msg.content.split(":")
         term = msgArray[0]
         definitions = msgArray[1]
@@ -50,7 +50,7 @@ async def reformat_dictionary_input(msg: discord.Message):
 
         await history_channel.send(f"Formatted on: ``{datetime.date.today().strftime('%d-%m-%Y %H:%M:%S')}``\n "
                                    f"New Message Link: {sent_message.jump_url}\n"
-                                   f"Original Content:\n{msg}")
+                                   f"Original Content:\n{msg.content}")
         # await history_channel.send(f"Created on: ``{datetime.date.today().strftime('%d-%m-%Y %H:%M:%S')}``\n "
         #                            f"Link: {sent_message.jump_url}\n"
         #                            f"Content:\n{sent_message}")
@@ -59,9 +59,9 @@ async def reformat_dictionary_input(msg: discord.Message):
         await history_channel.send(
             f"ERROR IN FORMATTING: please follow the format convention\n"
             f"<word>: <#>. <definition> [optional] <#>. <definition>\n"
-            f"example 1: あわてぃーはーてぃ: 1. hurry, in a hurry\n"
-            f"example 2: でーじ : 1. Really, very 2. truly, genuinely\n\n"
-            f"content: {msg}")
+            f"example 1. あわてぃーはーてぃ: 1. hurry, in a hurry\n"
+            f"example 2. でーじ : 1. Really, very 2. truly, genuinely\n\n"
+            f"content: {msg.content}")
         await msg.delete()
 
 
@@ -121,7 +121,36 @@ async def set_channel(msg: discord.Message):
 
 
 async def invalid_command(msg: discord.Message):
-    await msg.channel.send(f"```{msg}``` is not a valid command")
+    await msg.channel.send(f"```{msg.content}``` is not a valid command")
+
+async def set_channels_for_testing(bot_channel):
+    global dictionary
+    dictionary = "dictionary"
+    global dictionary_channel
+    dictionary_channel = discord.utils.get(bot_channel.guild.text_channels, name=dictionary)
+    global dictionary_id
+    dictionary_id = dictionary_channel.id
+
+    global phrases
+    phrases = "文と句"
+    global phrases_channel
+    phrases_channel = discord.utils.get(bot_channel.guild.text_channels, name=phrases)
+    global phrases_id
+    phrases_id = phrases_channel.id
+
+    global undefined_words
+    undefined_words = "needs-translation"
+    global undefined_words_channel
+    undefined_words_channel = discord.utils.get(bot_channel.guild.text_channels, name=undefined_words)
+    global undefined_words_id
+    undefined_words_id = undefined_words_channel.id
+
+    global history
+    history = "history"
+    global history_channel
+    history_channel = discord.utils.get(bot_channel.guild.text_channels, name=history)
+    global history_id
+    history_id = history_channel.id
 
 
 # Messages
